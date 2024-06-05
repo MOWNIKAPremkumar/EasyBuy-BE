962862ecc3fafe2cef6571a37144ee9d937275e3
MOWNIKAPremkumar <mowni02mani09@gmail.com>
2024-06-05 11:31:35 +0530

    second  commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 const path = require('path');
 const connectDatabase = require('./config/database');
-const cors = require('cors');
-app.use(cors);
-
 connectDatabase();
+const cors = require('cors');
+app.use(cors());
+
+
 
 const server = app.listen(process.env.PORT,()=>{
     console.log(`My Server listening to the port: ${process.env.PORT} in  ${process.env.NODE_ENV} `)
