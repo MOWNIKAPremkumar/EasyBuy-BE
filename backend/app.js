da0aaf8a8e19ae193b98985f8646a930b9eafec1
MOWNIKAPremkumar <mowni02mani09@gmail.com>
2024-06-05 17:51:35 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('*', (req, res) =>{
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-        res.send('EasyBuy E-Commerce Backend Running Successfully')
+        //res.send('EasyBuy E-Commerce Backend Running Successfully')
     })
 }
 
