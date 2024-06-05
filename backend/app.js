79abccf40b4eda9e17c16db388f39c18e1556160
MOWNIKAPremkumar <mowni02mani09@gmail.com>
2024-06-05 19:14:04 +0530

    changes committed

--- backend/app.js
+++ backend/app.js
@@ -24,10 +24,11 @@ app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
 if(process.env.NODE_ENV === "production") {
-    //app.use(express.static(path.join(__dirname, '../frontend/build')));
+    
+    app.use(express.static(path.join(__dirname, 'https://easy-buy-fe.vercel.app')));
     
     app.get('*', (req, res) =>{
-        //res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+       
         res.send('EasyBuy E-Commerce Backend Running Successfully')
     })
 }
