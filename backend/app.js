dadda2ba33e6e257c617d386fa4f0f89ead1ffa1
MOWNIKAPremkumar <mowni02mani09@gmail.com>
2024-06-05 11:30:58 +0530

    changed cors

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 const errorMiddleware = require('./middlewares/error');
 const cookieParser = require('cookie-parser')
 const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config({path:path.join(__dirname,"config/config.env")});
 
-
-
-// const corsOrigin ={
-//     origin:'FRONTEND_URL', 
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials:true,            
-//     optionSuccessStatus:200
-// }
-app.use(cors);
+//app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
